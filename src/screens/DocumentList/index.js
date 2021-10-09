d3b5151e2aef4b84b136a8ac6ebb4a44e425ba8f
nmzgnv <nick-mozg1@mail.ru>
2021-10-09 13:26:55 +0500

    Document type

--- src/screens/DocumentList/index.js
+++ src/screens/DocumentList/index.js
@@ -4,6 +4,7 @@ import {styles} from './styles';
 import Portfolio from '../../Components/Portfolio';
 import commonStyles from '../../MixIn/commonStyles';
 import {Header} from '../../Components/Header';
+import {DocumentType} from '../../Constants/Documents';
 
 const DocumentList = () => {
   return (
@@ -19,6 +20,7 @@ const DocumentList = () => {
             delta: '0.01%',
             description: 'Крутая облигация, покупай быстрее!',
             country: 'Россия',
+            type: DocumentType.bond,
           },
         ]}
       />
