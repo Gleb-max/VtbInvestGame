af6d0e914e422c8e2d29666dc96fefc487241f53
nmzgnv <nick-mozg1@mail.ru>
2021-10-09 12:58:46 +0500

    progress bar

--- src/Components/Portfolio/PortfolioItem.js
+++ src/Components/Portfolio/PortfolioItem.js
@@ -1,27 +1,30 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Text, TouchableOpacity, View} from 'react-native';
 import {styles} from './styles';
 import commonStyles from '../../MixIn/commonStyles';
-import {LogoImage} from '../../library/components';
 import {CircleImage} from '../CircleImage';
 
 export const PortfolioItem = props => {
   return (
-    <View style={styles.itemContainer}>
-      <View style={[commonStyles.horizontalContainer, {alignItems: 'center'}]}>
-        <CircleImage source={props.image} size={50} />
-        <View style={styles.textBlock}>
-          <Text style={styles.name}>{props.name}</Text>
-          <View
-            style={[
-              commonStyles.horizontalContainer,
-              {justifyContent: 'space-between'},
-            ]}>
-            <Text>{props.index}</Text>
-            <Text>{props.cost} $</Text>
+    <TouchableOpacity
+      onPress={() => console.warn('Implement: navigate to detail screen')}>
+      <View style={styles.itemContainer}>
+        <View
+          style={[commonStyles.horizontalContainer, {alignItems: 'center'}]}>
+          <CircleImage source={props.image} size={50} />
+          <View style={styles.textBlock}>
+            <Text style={styles.name}>{props.name}</Text>
+            <View
+              style={[
+                commonStyles.horizontalContainer,
+                {justifyContent: 'space-between'},
+              ]}>
+              <Text>{props.index}</Text>
+              <Text>{props.cost} $</Text>
+            </View>
           </View>
         </View>
       </View>
-    </View>
+    </TouchableOpacity>
   );
 };
--- src/screens/ProgressScreen/index.js
+++ src/screens/ProgressScreen/index.js
@@ -2,17 +2,15 @@
 import React from 'react';
 import {View} from 'react-native';
 import ProgressBlock from '../../Components/ProgressBlock';
-import commonStyles from '../../MixIn/commonStyles';
 import {styles} from './styles';
-import Dialogue from '../../Components/Dialogue';
 
 export const ProgressScreen = () => {
   return (
     <View style={[styles.container]}>
       <View style={styles.line} />
-      <ProgressBlock active={true} text={'Облигации'} />
-      <ProgressBlock active={false} text={'Фонды'} />
-      <ProgressBlock active={false} text={'Акции'} />
+      <ProgressBlock active={true} text={'Облигации'} description={'Купить облигацию — значит дать государству или компании в долг под проценты.'}/>
+      <ProgressBlock active={false} text={'Фонды'} description={'это такая «акция», которая содержит в себе несколько других акций. Покупая 1 акцию ETF, вы покупаете все входящие в него акции в определенной пропорции.'} />
+      <ProgressBlock active={false} text={'Акции'} description={'Акция — это часть компании, доля в ее бизнесе. Если вы купили акцию, то фактически стали совладельцем компании и можете претендовать на часть ее прибыли.\n'} />
     </View>
   );
 };
